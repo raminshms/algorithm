b0a36ab13b0d3f462d818a90d836a30dce0b0895
ramin.shamsi <ramin.shamsi@digikala.com>
2024-08-19 11:17:04 +0330

    solve container with most water problem

---
+++ neetcode-roadmap/two-pointers/container_with_most_water.py
@@ -0,0 +1,21 @@
+def maxArea(height):
+    l = 0
+    r = len(height) - 1
+    mostWater = 0
+    while l < r:
+        h = min(height[l], height[r])
+        length = r - l
+        mostWater = max(mostWater, h*length)
+
+        if height[l] <= height[r]:
+            l += 1
+        else:
+            r -= 1
+
+    return mostWater
+
+
+print(maxArea([1, 7, 2, 5, 4, 7, 3, 6]))
+print(maxArea([2, 2, 2]))
+print(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+print(maxArea([1, 1]))
